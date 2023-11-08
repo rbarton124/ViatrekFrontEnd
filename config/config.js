d48192c3d7939bfcccf01de0f873928b237378a5
rbarton124 <131946965+rbarton124@users.noreply.github.com>
2023-11-08 14:18:26 -0500

    Update config.js to use github secrets

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    googleApiKey: 'AIzaSyDd6noW5znBbemcByF-o7scCtpMZ-HqWi4',
+    googleApiKey: ${{secrets.GOOGLEIMAGES_APIKEY}},
     cx: '31a4a650f687741bf',
     OPENAI_ORG_ID: "org-tnB2P1d1CPtfcJSWNY0JGAct",
-    OPENAI_API_KEY: "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",
+    OPENAI_API_KEY: ${{secrets.OPENAI_APIKEY}},
   };
   
