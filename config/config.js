e884cf57481e08dfbfd6748afe516be56f520e0b
Russell Barton <barton5russell@gmail.com>
2023-11-08 14:26:04 -0500

    Update config.js to use environment variables instead of secrets.

--- config/config.js
+++ config/config.js
@@ -1,7 +1,6 @@
 module.exports = {
-    googleApiKey: ${{secrets.GOOGLEIMAGES_APIKEY}},
+    googleApiKey: process.env.GOOGLEIMAGES_APIKEY,
     cx: '31a4a650f687741bf',
     OPENAI_ORG_ID: "org-tnB2P1d1CPtfcJSWNY0JGAct",
-    OPENAI_API_KEY: ${{secrets.OPENAI_APIKEY}},
+    OPENAI_API_KEY: process.env.OPENAI_APIKEY,
   };
-  
